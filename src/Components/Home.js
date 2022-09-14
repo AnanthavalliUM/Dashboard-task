8470bc7cee895d2c2f8cc9c191bbe021702e4b42
ananthavalli <ananthavalli1993@gmail.com>
2022-09-14 22:37:23 +0530

    changed title

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,7 @@ import { Users } from "./Users";
 export const Home = () => {
   return (
     <>
-      <h1>UserList</h1>
+      <h1>Student's List</h1>
       {/* displaying Users in Home */}
       <Users />  
     </>
